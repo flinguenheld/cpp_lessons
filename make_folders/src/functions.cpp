4d4395a604a69d97eef4e04043ff607c4e1cc094
Florent <f@linguenheld.fr>
2023-05-31 23:49:12 +0200

    Add Makefile with folders

---
+++ make_folders/src/functions.cpp
@@ -0,0 +1,3 @@
+#include "../include/functions.h"
+
+void print_hello_world() { std::cout << "Hello world!!\n"; }
---
+++ make_folders/src/main.cpp
@@ -0,0 +1,7 @@
+#include "../include/functions.h"
+
+int main(int argc, char *argv[]) {
+
+  print_hello_world();
+  return 0;
+}
